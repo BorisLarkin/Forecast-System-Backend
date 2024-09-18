2e48acaf28e18612fb51476f90c6bd3d7b34780d
ITManiac <borislarkin18@mail.ru>
2024-09-18 20:17:49 +0300

    handling done

--- internal/api/server.go
+++ internal/api/server.go
@@ -18,30 +18,72 @@ type Forecast struct {
 	color       string
 }
 
-var Forecasts []Forecast
+var Forecasts []Forecast = []Forecast{
+	{
+		id:          1,
+		title:       "Прогноз температуры",
+		short_title: "Температура",
+		desc:        "Предскажем температуру посредством применения метода авторегрессии",
+		color:       "FFC3B6",
+		img_url:     "http://127.0.0.1:9000/test/source_obj/temp.png",
+	},
+	{
+		id:          2,
+		title:       "Предсказать давление",
+		short_title: "Давление",
+		desc:        "Покажем в мм рт. ст. наиболее вероятного значения атмосферного давления",
+		color:       "D5CEFF",
+		img_url:     "http://127.0.0.1:9000/test/source_obj/pressure.png",
+	},
+	{
+		id:          3,
+		title:       "Предугадать влажность",
+		short_title: "Влажность",
+		desc:        "Подскажем, как одеться по влажности атмосферного воздуха, в процентах",
+		color:       "DFE5FF",
+		img_url:     "http://127.0.0.1:9000/test/source_obj/humidity.png",
+	},
+}
 
 type Prediction struct {
 	id        int
-	f_id      int
+	forecast  Forecast
 	date_time string
 	place     string
-	f_type    Forecast
 }
 
-var Predictions [3]Prediction
+var Predictions []Prediction = []Prediction{
+	{
+		id:        1,
+		date_time: "18.09.2024, 19:54",
+		place:     "Москва",
+		forecast:  Forecasts[0],
+	},
+	{
+		id:        2,
+		date_time: "17.09.2024, 14:55",
+		place:     "Санкт-Петербург",
+		forecast:  Forecasts[2],
+	},
+	{
+		id:        3,
+		date_time: "20.10.2024, 00:43",
+		place:     "Москва",
+		forecast:  Forecasts[1],
+	},
+}
 
 func StartServer() {
 
 	jsonForecasts, err := os.ReadFile("forecasts.json")
+	if err != nil {
+		log.Print(err)
+	}
 	jsonPredictions, err := os.ReadFile("predictions.json")
 	json.Unmarshal(jsonForecasts, &Forecasts)
 	json.Unmarshal(jsonPredictions, &Predictions)
 	if err != nil {
 		log.Print(err)
-	}
-	for i, p := range Predictions {
-		p.f_type = Forecasts[p.f_id]
-		log.Println("got request ", i)
 	}
 
 	log.Println("Server start up")
@@ -57,180 +99,22 @@ func StartServer() {
 	})
 
 	r.GET("/cart", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "menu.tmpl", gin.H{
-			"Forecasts": Forecasts,
+		c.HTML(http.StatusOK, "cart.tmpl", gin.H{
+			"Forecasts": Predictions,
 		})
 	})
 
-	r.GET("/desc", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "menu.tmpl", gin.H{
-			"Forecasts": Forecasts,
+	r.GET("/details", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "details.tmpl", gin.H{
+			"id":          1,
+			"title":       "Прогноз температуры",
+			"short_title": "Температура",
+			"desc":        "Предскажем температуру посредством применения метода авторегрессии",
+			"color":       "FFC3B6",
+			"img_url":     "http://127.0.0.1:9000/test/source_obj/temp.png",
 		})
 	})
-
-	r.Static("/image", "./resources")
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	log.Println("Server down")
 }
-
-/*
-package main
-
-
-import (
-
-	"html/template"
-
-	"log"
-
-	"net/http"
-
-	"os"
-
-	"regexp"
-
-)
-
-
-type Page struct {
-
-	Title string
-
-	Body  []byte
-
-}
-
-
-func (p *Page) save() error {
-
-	filename := p.Title + ".txt"
-
-	return os.WriteFile(filename, p.Body, 0600)
-
-}
-
-
-func loadPage(title string) (*Page, error) {
-
-	filename := title + ".txt"
-
-	body, err := os.ReadFile(filename)
-
-	if err != nil {
-
-		return nil, err
-
-	}
-
-	return &Page{Title: title, Body: body}, nil
-
-}
-
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-
-	p, err := loadPage(title)
-
-	if err != nil {
-
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-
-		return
-
-	}
-
-	renderTemplate(w, "view", p)
-
-}
-
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-
-	p, err := loadPage(title)
-
-	if err != nil {
-
-		p = &Page{Title: title}
-
-	}
-
-	renderTemplate(w, "edit", p)
-
-}
-
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-
-	body := r.FormValue("body")
-
-	p := &Page{Title: title, Body: []byte(body)}
-
-	err := p.save()
-
-	if err != nil {
-
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-
-		return
-
-	}
-
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-
-}
-
-
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-
-	if err != nil {
-
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-
-	}
-
-}
-
-
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		m := validPath.FindStringSubmatch(r.URL.Path)
-
-		if m == nil {
-
-			http.NotFound(w, r)
-
-			return
-
-		}
-
-		fn(w, r, m[2])
-
-	}
-
-}
-
-
-func main() {
-
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
-}
-*/
